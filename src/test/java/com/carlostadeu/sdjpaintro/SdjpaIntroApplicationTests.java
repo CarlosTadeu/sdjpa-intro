6764d9055dfa8598d99880f8ae3e04f3c7918343
Carlos Tadeu <carlos.santos@opus-software.com.br>
2021-10-27 11:01:10 -0300

    Initial commit

---
+++ src/test/java/com/carlostadeu/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlostadeu.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
